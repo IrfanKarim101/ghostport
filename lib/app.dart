6275c5c1493f6bae0657b42ba914149b83f64c47
Irfan Karim <irfankarimhaji1122@gmail.com>
2025-07-11 09:45:58 +0500

    Integrate theme management with Provider; update MaterialApp to support light and dark themes

--- lib/app.dart
+++ lib/app.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:ghostport/screens/home_screen.dart';
 import 'package:ghostport/screens/splash_screen.dart';
+import 'package:ghostport/services/theme_services.dart';
+import 'package:ghostport/theme/theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GhostPort',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.black,
-        textTheme: const TextTheme(),
-      ),
-      home: const SplashScreen(),
-      initialRoute: '/',
-      routes: {'/home': (context) => const HomeScreen()},
-      debugShowCheckedModeBanner: false,
+    return Consumer<ThemeService>(
+      builder: (context, themeService, child) {
+        return MaterialApp(
+          title: 'GhostPort',
+          theme: AppThemes.lightTheme,
+          darkTheme: AppThemes.darkTheme,
+          themeMode: themeService.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+          home: const SplashScreen(),
+          initialRoute: '/',
+          routes: {'/home': (context) => const HomeScreen()},
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:ghostport/app.dart';
+import 'package:ghostport/services/theme_services.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const App());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeService(),
+    child: const App(),
+  ));
 }
